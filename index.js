2d82a3a689f00c2a963e212818a23966940be821
Matthew <matthew_mcaleer@hotmail.com>
2022-03-05 00:41:51 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+main.remove()
+
+const newHeader = document.createElement('h1')
+
+document.body.appendChild(newHeader)
+
+newHeader.id = 'victory'
+
+document.getElementById('victory')
+
+let name = 'matt'
+
+victory.textContent = `${name} is the champion`
